8a41b2438bbc124fd981eb5ad8c335d0d036a763
Alex Lementuev <a.lementuev@gmail.com>
2019-05-16 13:56:00 -0700

    Fixed typo

--- mParticle-Apptentive/MPKitApptentive.h
+++ mParticle-Apptentive/MPKitApptentive.h
@@ -35,9 +35,9 @@
 
 
 /**
- Begins Apptentive SDK initialization. Does nothing is the SDK is already initialized.
+ Begins Apptentive SDK initialization. Does nothing if the SDK is already initialized.
 
- @return YES if SDK initialization was successful. NO - if SDK was already initialized or failed to initialize.
+ @return YES if SDK initialization was successful. NO - if the SDK was already initialized or failed to initialize.
  */
 + (BOOL)registerSDK;
 
