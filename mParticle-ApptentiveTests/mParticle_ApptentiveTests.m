cfc6af12043bff9b99e1565438daac2919ad9c2a
Alex Lementuev <a.lementuev@gmail.com>
2021-05-02 16:37:02 -0400

    Added testing

---
+++ mParticle-ApptentiveTests/mParticle_ApptentiveTests.m
@@ -0,0 +1,37 @@
+//
+//  mParticle_ApptentiveTests.m
+//  mParticle-ApptentiveTests
+//
+//  Created by Alex Lementuev on 5/2/21.
+//  Copyright © 2021 mParticle. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface mParticle_ApptentiveTests : XCTestCase
+
+@end
+
+@implementation mParticle_ApptentiveTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
